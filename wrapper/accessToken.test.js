dbc3f388d8ad453df0412e8601f7fbac93e6d8ec
chrisgrimble <chris.grimble@hmcts.net>
2020-01-30 09:39:18 +0000

    Added option to use openId endpoints to get token and user details.
    Add openId=true to idam config to use them.

--- wrapper/accessToken.test.js
+++ wrapper/accessToken.test.js
@@ -14,7 +14,7 @@ describe('accessToken', () => {
     request.post.restore();
   });
 
-  it('makes the request to obtain token', () => {
+  it('makes the request to obtain oauth token', () => {
     // Arrange.
     const options = { field: 'value' };
     args.idamApiUrl = 'some-url';
@@ -29,6 +29,25 @@ describe('accessToken', () => {
     expect(requestOptions).to.have.nested.property('auth.user', args.idamClientID);
     expect(requestOptions).to.have.nested.property('auth.pass', args.idamSecret);
     expect(requestOptions).to.have.nested.property('form.field', options.field);
-    expect(requestOptions.uri).to.contain(args.idamApiUrl);
+    expect(requestOptions.uri).to.equal(`${args.idamApiUrl}/oauth2/token`);
+  });
+
+  it('makes the request to obtain openId token', () => {
+    // Arrange.
+    const options = { field: 'value' };
+    args.idamApiUrl = 'some-url';
+    args.idamClientID = 'some-id';
+    args.idamSecret = 'some-secret';
+    args.openId = true;
+    // Act.
+    accessToken(options, args);
+    // Assert.
+    expect(request.post.calledOnce).to.equal(true);
+    const requestOptions = request.post.getCall(0).args.pop();
+    expect(requestOptions).to.have.property('json', true);
+    expect(requestOptions).to.have.nested.property('form.field', options.field);
+    expect(requestOptions).to.have.nested.property('form.client_id', args.idamClientID);
+    expect(requestOptions).to.have.nested.property('form.client_secret', args.idamSecret);
+    expect(requestOptions.uri).to.equal(`${args.idamApiUrl}/o/token`);
   });
 });
